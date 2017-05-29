2cffac40c2c9a6effb0c02714d1ceb79a71027d9
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-29 12:42:59 -0400

    Add fields, add auto upgrade

--- contact.rb
+++ contact.rb
@@ -4,20 +4,17 @@ require "mini_record"
 
 AcriveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
 
-class Contact < ActiveRecord::Base 
+class Contact < ActiveRecord::Base
 
-
-  # attr_accessor :first_name, :last_name, :email, :note
-
-  # attr_reader :id
-
-
-
+  field :first_name,  as: :string
+  field :last_name,   as: :string
+  field :email,       as: :string
+  field :note,        as: :text
 
   def full_name
-
     "#{first_name} #{last_name}"
-
   end
 
 end
+
+Contact.auto_upgrade!
