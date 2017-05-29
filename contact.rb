161c2fec00d94b7ea26514c57cef06407f2fa798
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-29 12:31:06 -0400

    Connect contact.rb to sqlite3 and active_record ORM tool w/minirecord tool

--- contact.rb
+++ contact.rb
@@ -1,3 +1,12 @@
+gem 'activerecord', "=4.2.7"
+require "active_record"
+require "mini_record"
+
+AcriveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
+
+
+
 class Contact
 
 # Attribute methods
